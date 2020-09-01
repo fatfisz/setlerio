ffb0c728a0db7191867025a06e0ce4e9f0cb07ef
fatfisz <fatfisz@gmail.com>
2020-09-02 01:22:13 +0200

    Add eslint-plugin-import (for style only)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,35 @@ module.exports = {
   root: true,
   extends: ['eslint:recommended', 'prettier', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['prettier', '@typescript-eslint'],
+  plugins: ['import', 'prettier', '@typescript-eslint'],
   rules: {
     'no-sparse-arrays': 'off',
     'no-useless-rename': 'warn',
     'object-shorthand': 'warn',
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
     strict: 'warn',
+
+    'import/extensions': 'warn',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-default-export': 'warn',
+    'import/no-duplicates': 'warn',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
 
     'prettier/prettier': [
       'warn',
@@ -49,6 +72,9 @@ module.exports = {
       files: ['rollup.config.js', 'rollup-plugins/**'],
       parserOptions: {
         sourceType: 'module',
+      },
+      rules: {
+        'import/no-default-export': 'off',
       },
     },
   ],
